f4278e37f84c51d84a23b150db6f2d3044e3802b
Monty Anderson <work@montyanderson.net>
2019-08-01 08:21:20 +0100

    Code written with Shawn

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,55 @@
+function clearItems() {
+	document.querySelector("#dir-list").innerHTML = "";
+}
+
+function addItem(dir, path = "") {
+	const li = document.createElement("li");
+	const span = document.createElement("span");
+	const i = document.createElement("i");
+	const a = document.createElement("a");
+
+	if(path !== "/" && path !== "") {
+	  a.href = `${path}/${dir}`;
+	} else {
+	  a.href = `${path}${dir}`;
+	}
+
+	if(dir.startsWith('/')) {
+	  a.onclick = () => {
+		getDirectories(`${path}${dir}`);
+	  };
+
+	  a.href = `#`;
+	}
+
+	if(dir === "..") {
+		a.onclick = () => {
+		  getDirectories(`${path.split("/").slice(0, -1).join("/")}`);
+		};
+
+		a.href = `#`;
+	}
+
+	a.textContent = dir;
+
+	i.className = dir.startsWith('/') || dir === '..' ? "far fa-folder icon-pad" : "far fa-file-alt icon-pad";
+	span.appendChild(i);
+	span.appendChild(a);
+	li.className = "list-group-item d-flex justify-content-between align-items-center";
+	li.appendChild(span);
+	document.querySelector("#dir-list").appendChild(li);
+}
+
 function getDirectories(path = "/") {
   fetch(`/directories?path=${path}`).then((res) => res.json()).then((res) => {
-    document.querySelector("#dir-list").innerHTML = "";
+    clearItems();
 
-    if(path !== "/" && path !== "") {
-      const li = document.createElement("li");
-      const span = document.createElement("span");
-      const i = document.createElement("i");
-      const a = document.createElement("a");
+	addItem('..');
 
-        a.onclick = () => {
-          getDirectories(`${path.split("/").slice(0, -1).join("/")}`);
-        };
+	res.sort();
 
-        a.href = `#`;
-
-      a.textContent = "..";
-
-      i.className = "far fa-folder icon-pad";
-      span.appendChild(i);
-      span.appendChild(a);
-      li.className = "list-group-item d-flex justify-content-between align-items-center";
-      li.appendChild(span);
-      document.querySelector("#dir-list").appendChild(li);
-    }
-
-    res.sort();
-
-    res.forEach((dir) => {
-
-      const li = document.createElement("li");
-      const span = document.createElement("span");
-      const i = document.createElement("i");
-      const a = document.createElement("a");
-
-      if(path !== "/" && path !== "") {
-        a.href = `${path}/${dir}`;
-      } else {
-        a.href = `${path}${dir}`;
-      }
-
-      if(dir.startsWith('/')) {
-        a.onclick = () => {
-          getDirectories(`${path.slice(1, -1)}${dir}`);
-        };
-
-        a.href = `#`;
-      }
-
-      a.textContent = dir;
-
-      i.className = dir.startsWith('/') ? "far fa-folder icon-pad" : "far fa-file-alt icon-pad";
-      span.appendChild(i);
-      span.appendChild(a);
-      li.className = "list-group-item d-flex justify-content-between align-items-center";
-      li.appendChild(span);
-      document.querySelector("#dir-list").appendChild(li);
+    res.forEach(dir => {
+		addItem(dir, path);
     });
   });
 }
